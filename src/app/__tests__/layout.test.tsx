e537fed36c34ab9d5c28d230e1f299da7c107848
yusukeMIYAUCHI <yusukemiyauchi@MBA.local>
2024-05-21 13:16:19 +0900

    update: テスト項目を追加

---
+++ src/app/__tests__/layout.test.tsx
@@ -0,0 +1,6 @@
+import { it, describe } from 'vitest';
+
+describe('RootLayout表示確認', () => {
+  // 「const inter = Inter({ subsets: ['latin'] });」箇所のテスト実装が不明なため一旦スキップ
+  it.todo('RootLayoutが表示されていること');
+});
