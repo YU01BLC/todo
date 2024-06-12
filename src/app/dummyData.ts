c60ad7c75cadefae21f06c52d72187065e9ae50e
yusukeMIYAUCHI <yusukemiyauchi@MBA.local>
2024-06-12 23:53:56 +0900

    add: todoListコンポーネント表示部(モーダル以外)実装

---
+++ src/app/dummyData.ts
@@ -0,0 +1,44 @@
+export const dummyData = [
+  {
+    id: '1',
+    title: '今日のタスク',
+    person: '三上',
+    status: '着手中',
+    dueDate: '20XX-01-01',
+  },
+  {
+    id: '2',
+    title: '会議メモ',
+    person: '田中',
+    status: '未着手',
+    dueDate: '20XX-02-15',
+  },
+  {
+    id: '3',
+    title: 'やりたいこと',
+    person: '斉藤',
+    status: '着手中',
+    dueDate: '20XX-01-30',
+  },
+  {
+    id: '4',
+    title: '検討中リスト',
+    person: 'ジャック',
+    status: '完了',
+    dueDate: '20XX-01-30',
+  },
+  {
+    id: '5',
+    title: '筋トレメニュー',
+    person: '風呂屋',
+    status: '完了',
+    dueDate: '20XX-01-30',
+  },
+  {
+    id: '6',
+    title: '内定辞退メール下書き',
+    person: 'マルコビッチ',
+    status: '着手中',
+    dueDate: '20XX-01-30',
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,45 @@
+import React from 'react';
+import Image from 'next/image';
+
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
+
+import { dummyData } from './dummyData';
+
 export default function RootPage() {
-  return <div>hello world</div>;
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="text-right">
+        <Button variant="outline">アイテム追加</Button>
+      </div>
+      <div className="flex justify-center">
+        <Table className="border">
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-2/5">タイトル</TableHead>
+              <TableHead>担当者</TableHead>
+              <TableHead>ステータス</TableHead>
+              <TableHead>期限</TableHead>
+              <TableHead>アクション</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {dummyData.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell className="w-2/5">{item.title}</TableCell>
+                <TableCell>{item.person}</TableCell>
+                <TableCell>{item.status}</TableCell>
+                <TableCell>{item.dueDate}</TableCell>
+                <TableCell className="flex">
+                  <Image width={30} height={30} className="mr-4 mt-2" src="/image/pencil.svg" alt="pencil" />
+                  <Image width={20} height={20} src="/image/trash.svg" alt="trash" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </section>
+  );
 }
